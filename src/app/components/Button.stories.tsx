408e6674dca9ed74ef1e47ad0d2b199e7754b0ec
Rohit Shrestha <stha.rht028@gmail.com>
2020-07-24 20:01:33 +0545

    chore(button): create button component

--- src/app/components/Button.stories.tsx
+++ src/app/components/Button.stories.tsx
@@ -11,6 +11,11 @@ export default {
 export const Primary = () => <Button>Primary</Button>;
 export const Secondary = () => <Button type={"secondary"}>Secondary</Button>;
 export const Disabled = () => <Button disabled={true}>Disabled</Button>;
+export const Success = () => <Button type="success">Success</Button>;
+export const Error = () => <Button type="error">Error</Button>;
+export const Small = () => <Button btnSize="small">Small</Button>;
+export const Default = () => <Button>Default</Button>;
+export const Large = () => <Button btnSize="large">Large</Button>;
 export const StyledButton = () => {
   const style = {
     backgroundColor: Color.BLACK,
--- src/app/pages/HomePage.tsx
+++ src/app/pages/HomePage.tsx
@@ -1,13 +1,14 @@
 import * as React from "react";
-import {connect} from "react-redux";
-import {createSelector} from "reselect";
-import {stylesheet} from "typestyle";
-import {Color} from "../constants/Color";
+import { connect } from "react-redux";
+import { createSelector } from "reselect";
+import { stylesheet } from "typestyle";
+import { Button } from "../components/Button";
+import { Color } from "../constants/Color";
 import crazyImage from "../images/crazy.png";
-import {Translator} from "../models/Translator";
-import {ITranslator} from "../models/TranslatorInterfaces";
-import {IStore} from "../redux/IStore";
-import {translationsSelector} from "../selectors/translationsSelector";
+import { Translator } from "../models/Translator";
+import { ITranslator } from "../models/TranslatorInterfaces";
+import { IStore } from "../redux/IStore";
+import { translationsSelector } from "../selectors/translationsSelector";
 
 const classNames = stylesheet({
   container: {
@@ -24,13 +25,26 @@ interface IStateToProps {
 
 class HomePage extends React.Component<IStateToProps> {
   public render(): JSX.Element {
-    const {translations} = this.props;
+    const { translations } = this.props;
     return (
       <div className={classNames.container}>
         <a href={"https://www.crazy-factory.com"}>
           <img alt={"crazy logo"} src={crazyImage}/>
         </a>
         <p>{translations.hello}</p>
+        <Button btnSize="small">small</Button>
+        &nbsp;
+        <Button>default</Button>
+        &nbsp;
+        <Button btnSize="large">large</Button>
+        &nbsp;
+        <Button type="primary">primary</Button>
+        &nbsp;
+        <Button type="secondary">secondary</Button>
+        &nbsp;
+        <Button type="error">error</Button>
+        &nbsp;
+        <Button type="success">success</Button>
       </div>
     );
   }
@@ -53,4 +67,8 @@ function mapStateToProps(state: Pick<IStore, "settings">): IStateToProps {
 }
 
 const connected = connect(mapStateToProps)(HomePage);
-export {connected as HomePage, HomePage as UnconnectedHomePage, mapStateToProps};
+export {
+  connected as HomePage,
+  HomePage as UnconnectedHomePage,
+  mapStateToProps
+};
