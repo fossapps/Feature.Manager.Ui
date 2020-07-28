55551eca96255df0ddcabe59035f5a90f2bd4ae5
Nishchal <gautam.nishchal@gmail.com>
2020-07-28 19:06:02 +0700

    test(*): fix tests

--- src/app/pages/HomePage.test.tsx
+++ src/app/pages/HomePage.test.tsx
@@ -1,30 +1,18 @@
-import { shallow } from "enzyme";
-import * as React from "react";
-import { ISettingsState } from "../redux/modules/settingsModule";
-import { mapStateToProps, UnconnectedHomePage } from "./HomePage";
+import { IFeatureState } from "../redux/modules/features/featureModule";
+import { mapStateToProps } from "./HomePage";
 describe("<HomePage />", () => {
-  const translations = {
-    hello: "Hello!"
-  };
-
   it("maps state to props correctly", () => {
-    const settings: ISettingsState = {
-      error: "",
-      language: "en",
+    const features: IFeatureState = {
+      error: null,
+      features: [],
       loaded: true,
-      pending: false,
-      translations: { Hello: "Hello!" }
+      pending: false
     };
-    const props = mapStateToProps({ settings });
+    const props = mapStateToProps({ features });
     expect(props).toEqual({
       translations: {
         hello: "Hello!"
       }
     });
-  });
-
-  it("says hello", () => {
-    const wrapper = shallow(<UnconnectedHomePage translations={translations}/>);
-    expect(wrapper.find("p")).toHaveText("Hello!");
   });
 });
